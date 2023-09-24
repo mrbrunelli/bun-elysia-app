2b094bf1e01fb091aa13c9dd5e749ab546320708
mrbrunelli <matheus.brunelli@gmail.com>
2023-09-24 00:57:07 -0300

    test: fix tests and use correct interface to body mock

--- tests/main/routes/signup.spec.ts
+++ tests/main/routes/signup.spec.ts
@@ -1,19 +1,50 @@
 import { describe, expect, it } from "bun:test";
 import { app } from "../../../src/main/app";
 
-describe("signup routes", () => {
-  it("POST /signup", async () => {
-    const response = await app.handle(
-      new Request("http://localhost/signup", {
-        method: "POST",
-        body: JSON.stringify({
-          username: "valid-username",
-          password: "valid-password",
-        }),
-      })
-    );
+describe("SignUp routes", () => {
+  describe("POST /signup", () => {
+    const url = "http://localhost/signup";
 
-    expect(response.status).toBe(201);
-    expect(await response.text()).toBe("user registered successfully");
+    it("should return 201 with user success message", async () => {
+      const response = await app.handle(
+        new Request({
+          url,
+          method: "POST",
+          body: new URLSearchParams({
+            username: "valid-username",
+            password: "valid-password",
+          }),
+        })
+      );
+
+      expect(response.status).toBe(201);
+      expect(await response.text()).toBe("user registered successfully");
+    });
+
+    it("should return 400 if username is missing", async () => {
+      const response = await app.handle(
+        new Request(url, {
+          method: "POST",
+          body: new URLSearchParams({
+            password: "valid-password",
+          }),
+        })
+      );
+
+      expect(response.status).toBe(400);
+    });
+
+    it("should return 400 if password is missing", async () => {
+      const response = await app.handle(
+        new Request(url, {
+          method: "POST",
+          body: new URLSearchParams({
+            username: "valid-username",
+          }),
+        })
+      );
+
+      expect(response.status).toBe(400);
+    });
   });
 });
